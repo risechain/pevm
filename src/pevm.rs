d9e9599b30fe3b76f005d7c008a4bab297cad8f9
Hai Nguyen Quang <hai@caliber.build>
2024-07-14 12:18:14 +0700

    perf: fine-tune sequential fallback condition

--- src/pevm.rs
+++ src/pevm.rs
@@ -86,7 +86,10 @@ pub fn execute<S: Storage + Send + Sync, C: PevmChain + Send + Sync>(
         _ => return Err(PevmError::MissingTransactionData),
     };
     // TODO: Continue to fine tune this condition.
-    if force_sequential || tx_envs.len() < 4 || block.header.gas_used < 2_000_000 {
+    if force_sequential
+        || tx_envs.len() < concurrency_level.into()
+        || block.header.gas_used < 4_000_000
+    {
         execute_revm_sequential(storage, chain, spec_id, block_env, tx_envs)
     } else {
         execute_revm_parallel(
