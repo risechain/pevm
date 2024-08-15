cc2d24a231edee140dd393df774b31e2a6ab8b34
Kien Nguyen <kien@caliber.build>
2024-08-15 11:44:15 +0700

    debug: store storage as a separated table

--- src/storage/on_disk.rs
+++ src/storage/on_disk.rs
@@ -1,20 +1,23 @@
 use std::{path::Path, thread::ThreadId};
 
-use alloy_primitives::{keccak256, Address, B256, B64, U256};
+use alloy_consensus::constants::KECCAK_EMPTY;
+use alloy_primitives::{keccak256, Address, FixedBytes, B256, B64, U256};
 use dashmap::{DashMap, Entry, OccupiedEntry};
 use reth_libmdbx::{Database, Environment, Transaction, RO};
 
-use super::{AccountBasic, EvmAccount, EvmCode, Storage};
+use super::{AccountBasic, EvmCode, Storage};
 
 /// A storage that reads data from an on-disk MDBX database.
 #[derive(Debug)]
 pub struct OnDiskStorage {
     env: Environment,
-    cache_accounts: DashMap<Address, Option<EvmAccount>>,
+    cache_encoded_accounts: DashMap<Address, Option<(B256, B64, B256)>>,
+    cache_storage: DashMap<(Address, U256), U256>,
     cache_bytecodes: DashMap<B256, Option<EvmCode>>,
     cache_block_hashes: DashMap<u64, B256>,
     cache_txs: DashMap<ThreadId, Transaction<RO>>,
-    table_accounts: Database,
+    table_encoded_accounts: Database,
+    table_storage: Database,
     table_bytecodes: Database,
     table_block_hashes: Database,
 }
@@ -24,44 +27,48 @@ impl OnDiskStorage {
     pub fn open(path: impl AsRef<Path>) -> Result<Self, reth_libmdbx::Error> {
         let env = Environment::builder().set_max_dbs(16).open(path.as_ref())?;
         let tx = env.begin_ro_txn()?;
-        let table_accounts = tx.open_db(Some("accounts"))?;
+        // let table_accounts = tx.open_db(Some("accounts"))?;
+        let table_encoded_accounts = tx.open_db(Some("encoded_accounts"))?;
+        let table_storage = tx.open_db(Some("storage"))?;
         let table_bytecodes = tx.open_db(Some("bytecodes"))?;
         let table_block_hashes = tx.open_db(Some("block_hashes"))?;
         Ok(Self {
             env,
-            cache_accounts: DashMap::default(),
+            cache_encoded_accounts: DashMap::default(),
+            cache_storage: DashMap::default(),
             cache_bytecodes: DashMap::default(),
             cache_block_hashes: DashMap::default(),
             cache_txs: DashMap::default(),
-            table_accounts,
+            table_encoded_accounts,
+            table_storage,
             table_bytecodes,
             table_block_hashes,
         })
     }
 
-    fn load_account(
+    fn load_encoded_account(
         &self,
         address: Address,
-    ) -> Result<OccupiedEntry<Address, Option<EvmAccount>>, reth_libmdbx::Error> {
-        match self.cache_accounts.entry(address) {
+    ) -> Result<OccupiedEntry<Address, Option<(B256, B64, B256)>>, reth_libmdbx::Error> {
+        match self.cache_encoded_accounts.entry(address) {
             Entry::Occupied(occupied) => Ok(occupied),
             Entry::Vacant(vacant) => {
                 let tx_ref = self
                     .cache_txs
                     .entry(std::thread::current().id())
                     .or_insert_with(|| self.env.begin_ro_txn().unwrap());
-                let bytes: Option<Vec<u8>> = tx_ref
+                let bytes: Option<[u8; 32 + 8 + 32]> = tx_ref
                     .value()
-                    .get(self.table_accounts.dbi(), address.as_ref())?;
+                    .get(self.table_encoded_accounts.dbi(), address.as_ref())?;
                 drop(tx_ref);
-                let account: Option<EvmAccount> = match bytes {
-                    Some(bytes) => Some(
-                        bincode::deserialize(bytes.as_slice())
-                            .map_err(|_| reth_libmdbx::Error::DecodeError)?,
-                    ),
-                    None => None,
-                };
-                Ok(vacant.insert_entry(account))
+
+                let decoded = bytes.map(|bytes| {
+                    let b = B256::from_slice(&bytes[0..32]);
+                    let n = B64::from_slice(&bytes[32..(32 + 8)]);
+                    let c = B256::from_slice(&bytes[(32 + 8)..(32 + 8 + 32)]);
+                    (b, n, c)
+                });
+                Ok(vacant.insert_entry(decoded))
             }
         }
     }
@@ -71,16 +78,19 @@ impl Storage for OnDiskStorage {
     type Error = reth_libmdbx::Error;
 
     fn basic(&self, address: &Address) -> Result<Option<AccountBasic>, Self::Error> {
-        let entry = self.load_account(*address)?;
+        let entry = self.load_encoded_account(*address)?;
         Ok(entry.get().as_ref().map(|account| AccountBasic {
-            balance: account.balance,
-            nonce: account.nonce,
+            balance: account.0.into(),
+            nonce: account.1.into(),
         }))
     }
 
     fn code_hash(&self, address: &Address) -> Result<Option<B256>, Self::Error> {
-        let entry = self.load_account(*address)?;
-        Ok(entry.get().as_ref().and_then(|account| account.code_hash))
+        let entry = self.load_encoded_account(*address)?;
+        Ok(entry
+            .get()
+            .as_ref()
+            .and_then(|(_, _, c)| (*c != KECCAK_EMPTY).then_some(*c)))
     }
 
     fn code_by_hash(&self, code_hash: &B256) -> Result<Option<EvmCode>, Self::Error> {
@@ -108,22 +118,34 @@ impl Storage for OnDiskStorage {
         }
     }
 
-    fn has_storage(&self, address: &Address) -> Result<bool, Self::Error> {
-        let entry = self.load_account(*address)?;
-        Ok(entry
-            .get()
-            .as_ref()
-            .map(|account| !account.storage.is_empty())
-            .unwrap_or_default())
+    fn has_storage(&self, _address: &Address) -> Result<bool, Self::Error> {
+        Ok(false)
     }
 
     fn storage(&self, address: &Address, index: &U256) -> Result<U256, Self::Error> {
-        let entry = self.load_account(*address)?;
-        Ok(entry
-            .get()
-            .as_ref()
-            .and_then(|account| account.storage.get(index).copied())
-            .unwrap_or_default())
+        match self.cache_storage.entry((*address, *index)) {
+            Entry::Occupied(occupied) => Ok(*occupied.get()),
+            Entry::Vacant(vacant) => {
+                let tx_ref = self
+                    .cache_txs
+                    .entry(std::thread::current().id())
+                    .or_insert_with(|| self.env.begin_ro_txn().unwrap());
+                let bytes: Option<[u8; 32]> = tx_ref.value().get(
+                    self.table_storage.dbi(),
+                    FixedBytes::<{ 20 + 32 }>::from_slice(
+                        &[address.as_slice(), Into::<B256>::into(*index).as_slice()].concat(),
+                    )
+                    .as_ref(),
+                )?;
+                drop(tx_ref);
+                let storage_value = match bytes {
+                    Some(bytes) => U256::from_be_bytes(bytes),
+                    None => U256::ZERO,
+                };
+                vacant.insert(storage_value);
+                Ok(storage_value)
+            }
+        }
     }
 
     fn block_hash(&self, number: &u64) -> Result<B256, Self::Error> {
--- tests/common/mdbx.rs
+++ tests/common/mdbx.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
+use std::{collections::HashMap, path::Path};
 
-use alloy_primitives::{Address, B256, B64};
+use alloy_primitives::{b256, Address, B256, B64};
 use pevm::{EvmAccount, EvmCode};
 use reth_libmdbx::{
     DatabaseFlags, Environment, EnvironmentFlags, Geometry, Mode, SyncMode, WriteFlags,
@@ -37,6 +37,10 @@ fn write_table_to<K: AsRef<[u8]>, V: AsRef<[u8]>>(
     Ok(())
 }
 
+/// The Keccak-256 hash of the empty string `""`.
+const KECCAK_EMPTY: B256 =
+    b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+
 /// Create a temp dir containing MDBX
 pub(crate) fn create_db_dir<'a>(
     dir: impl AsRef<Path>,
@@ -50,11 +54,46 @@ pub(crate) fn create_db_dir<'a>(
         "bytecodes",
         bytecodes.map(|(code_hash, evm_code)| (code_hash, bincode::serialize(&evm_code).unwrap())),
     )?;
+
+    // balance, nonce, code_hash (default: KECCAK_EMPTY)
+    let mut encoded_accounts = HashMap::<Address, (B256, B64, B256)>::new();
+    let mut storage = HashMap::<(Address, B256), B256>::new();
+    for (&address, account) in pre_state {
+        encoded_accounts.insert(
+            address,
+            (
+                B256::from(account.balance),
+                B64::from(account.nonce),
+                account.code_hash.unwrap_or(KECCAK_EMPTY),
+            ),
+        );
+        for (&index, &storage_value) in account.storage.iter() {
+            storage.insert((address, B256::from(index)), B256::from(storage_value));
+        }
+    }
+
     write_table_to(
         &env,
-        "accounts",
-        pre_state.map(|(address, account)| (address, bincode::serialize(account).unwrap())),
+        "encoded_accounts",
+        encoded_accounts.into_iter().map(|(address, (b, n, c))| {
+            (address, [b.as_slice(), n.as_slice(), c.as_slice()].concat())
+        }),
     )?;
+
+    write_table_to(
+        &env,
+        "storage",
+        storage
+            .into_iter()
+            .map(|((a, i), v)| ([a.as_slice(), i.as_slice()].concat(), v)),
+    )?;
+
+    // write_table_to(
+    //     &env,
+    //     "accounts",
+    //     pre_state.map(|(address, account)| (address, bincode::serialize(account).unwrap())),
+    // )?;
+
     write_table_to(
         &env,
         "block_hashes",
