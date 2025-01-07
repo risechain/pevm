71f6111be91839490b5dad2f304449894353fa5c
Vansh <134165003+Levi0804@users.noreply.github.com>
2025-01-07 19:57:41 +0530

    Add `snapshot_data.rs` (#455)

---
+++ crates/pevm/tests/snapshot_data.rs
@@ -0,0 +1,34 @@
+//! Test if snapshotted mainnet data is correct
+
+use alloy_primitives::B256;
+use alloy_provider::{Provider, ProviderBuilder};
+use alloy_rpc_types_eth::{BlockNumberOrTag, BlockTransactionsKind};
+use std::collections::BTreeMap;
+use std::fs::File;
+
+#[tokio::test]
+async fn snapshotted_mainnet_block_hashes() {
+    let file = File::open("../../data/block_hashes.bincode").unwrap();
+    let block_hashes = bincode::deserialize_from::<_, BTreeMap<u64, B256>>(file).unwrap();
+
+    let rpc_url = match std::env::var("ETHEREUM_RPC_URL") {
+        // The empty check is for GitHub Actions where the variable is set with an empty string when unset!?
+        Ok(value) if !value.is_empty() => value.parse().unwrap(),
+        _ => reqwest::Url::parse("https://eth.public-rpc.com").unwrap(),
+    };
+
+    let provider = ProviderBuilder::new().on_http(rpc_url);
+
+    for (block_number, snapshotted_hash) in block_hashes {
+        let block = provider
+            .get_block_by_number(
+                BlockNumberOrTag::Number(block_number),
+                BlockTransactionsKind::Hashes,
+            )
+            .await
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(snapshotted_hash, block.header.hash);
+    }
+}
