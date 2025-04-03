01b051c4cce52b920562ce053628dc6f30210570
Ragnar <rodiondenmark@gmail.com>
2025-04-03 12:19:33 +0200

    Update mainnet.rs

--- crates/pevm/tests/mainnet.rs
+++ crates/pevm/tests/mainnet.rs
@@ -77,7 +77,7 @@ async fn optimism_mainnet_blocks_from_rpc() {
                   // 117874236, // ECOTONE (https://specs.optimism.io/protocol/ecotone/overview.html)
                   // 122874325, // FJORD (https://specs.optimism.io/protocol/fjord/overview.html)
                   125874340, // GRANITE (https://specs.optimism.io/protocol/granite/overview.html)
-                  // TODO: Add test for HOLOCENE (to be activated on Jan 9 2025 - timestamp 1736445601)
+                  130423412, // HOLOCENE (https://specs.optimism.io/protocol/holocene/overview.html)
     ] {
         let provider = ProviderBuilder::new()
             .network::<op_alloy_network::Optimism>()
